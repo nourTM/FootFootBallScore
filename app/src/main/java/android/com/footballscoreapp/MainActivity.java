f5667ce09a2b6d860e2555dedff04f870326152b
Nour Elhouda Ben Saadi <nh.bensaadi@esi-sba.dz>
2021-01-05 23:40:41 +0100

    Initial commit

---
+++ app/src/main/java/android/com/footballscoreapp/MainActivity.java
@@ -0,0 +1,78 @@
+package android.com.footballscoreapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Tracks the score for Team A
+    int scoreTeamA = 0;
+
+    // Tracks the score for Team B
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * Increase the score for Team A by 1 points.
+     */
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+
+    /**
+     * Increase the score for Team B by 1 points.
+     */
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Resets the score for both teams back to 0.
+     */
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void playerFoul(View view) {
+        // #C70039
+        // #DAF7A6 green
+        ColorDrawable viewColor = (ColorDrawable) view.getBackground();
+        int colorId = viewColor.getColor();
+        if( colorId == Color.parseColor("#DAF7A6"))((Button) view).setBackgroundColor(Color.parseColor("#FFC300"));
+        else if (colorId == Color.parseColor("#FFC300")) ((Button) view).setBackgroundColor(Color.parseColor("#C70039"));
+    }
+}
